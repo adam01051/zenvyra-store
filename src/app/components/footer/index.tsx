e18532277e7a8e6133e2955ce6c947feaa97ffc9
adam01051 <lloki0105@gmail.com>
2026-04-29 20:36:17 +0900

    feat: developed new footer section

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,89 +1,99 @@
 import React from "react";
 import { Box, Container, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const Footers = styled.div`
-  width: 100%;
-  height: 590px;
-  display: flex;
-  background: #343434;
-  background-size: cover;
-`;
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 export default function Footer() {
-  const authMember = null;
+	return (
+		<div className="footer-section">
+			<Container>
+				{/* 1. NEWSLETTER CARD (Overlays the top of footer) */}
+				<Box className="newsletter-card">
+					<Box className="newsletter-text">
+						STAY UP TO DATE ABOUT OUR LATEST OFFERS
+					</Box>
+					<Stack className="newsletter-inputs">
+						<div className="input-field">
+							<MailOutlineIcon sx={{ color: "rgba(0,0,0,0.4)" }} />
+							<input type="text" placeholder="Enter your email address" />
+						</div>
+						<button className="subscribe-btn">Subscribe to Newsletter</button>
+					</Stack>
+				</Box>
 
-  return (
-    <Footers>
-      <Container>
-        <Stack flexDirection={"row"} sx={{ mt: "94px" }}>
-          <Stack flexDirection={"column"} style={{ width: "340px" }}>
-            <Box>
-              <img width={"100px"} src={"/icons/burak.svg"} alt="Burak Logo" />
-            </Box>
-            <Box className={"foot-desc-txt"}>
-              Focusing on the gourmet Turkish breakfast as well as the youth
-              society, CZN Burak Gurme aims to bring Turkish cuisine back. CZN
-              Burak Gurme creates an illusion with its cuisine.
-            </Box>
-            <Box className="sns-context">
-              <img src={"/icons/facebook.svg"} />
-              <img src={"/icons/twitter.svg"} />
-              <img src={"/icons/instagram.svg"} />
-              <img src={"/icons/youtube.svg"} />
-            </Box>
-          </Stack>
-          <Stack sx={{ ml: "288px" }} flexDirection={"row"}>
-            <Stack>
-              <Box>
-                <Box className={"foot-category-title"}>Bo'limlar</Box>
-                <Box className={"foot-category-link"}>
-                  <Link to="/">Home</Link>
-                  <Link to="/products">Products</Link>
-                  {authMember && <Link to="/orders">Orders</Link>}
-                  <Link to="/help">Help</Link>
-                </Box>
-              </Box>
-            </Stack>
-            <Stack sx={{ ml: "100px" }}>
-              <Box>
-                <Box className={"foot-category-title"}>Find us</Box>
-                <Box
-                  flexDirection={"column"}
-                  sx={{ mt: "20px" }}
-                  className={"foot-category-link"}
-                  justifyContent={"space-between"}
-                >
-                  <Box flexDirection={"row"} className={"find-us"}>
-                    <span>L.</span>
-                    <div>Downtown, Dubai</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>P.</span>
-                    <div>+971 4 554 7777</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>E.</span>
-                    <div>devexuz@gmail.com</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>H.</span>
-                    <div>Visit 24 hours</div>
-                  </Box>
-                </Box>
-              </Box>
-            </Stack>
-          </Stack>
-        </Stack>
-        <Stack
-          style={{ border: "1px solid #C5C8C9", width: "100%", opacity: "0.2" }}
-          sx={{ mt: "80px" }}
-        ></Stack>
-        <Stack className={"copyright-txt"}>
-          © Copyright Devex Global, All rights reserved.
-        </Stack>
-      </Container>
-    </Footers>
-  );
+				{/* 2. MAIN FOOTER CONTENT */}
+				<Stack
+					direction="row"
+					justifyContent="space-between"
+				
+				>
+					{/* Brand Info */}
+					<Stack spacing={2} style={{ width: "248px" }}>
+						<Box className="footer-logo">ZENVYRA</Box>
+						<Box className="foot-desc-txt">
+							We have clothes that suits your style and which you’re proud to
+							wear. From women to men.
+						</Box>
+						<Box className="sns-context">
+							<img src="/icons/twitter.svg" alt="twitter" />
+							<img src="/icons/facebook.svg" alt="facebook" />
+							<img src="/icons/instagram.svg" alt="instagram" />
+							<img src="/icons/github.svg" alt="github" />
+						</Box>
+					</Stack>
+
+					{/* Links Columns */}
+					<Stack direction="row" spacing={10}>
+						<Box className="foot-category-link">
+							<Box className="foot-category-title">COMPANY</Box>
+							<Link to="/about">About</Link>
+							<Link to="/features">Features</Link>
+							<Link to="/works">Works</Link>
+							<Link to="/career">Career</Link>
+						</Box>
+						<Box className="foot-category-link">
+							<Box className="foot-category-title">HELP</Box>
+							<Link to="/support">Customer Support</Link>
+							<Link to="/delivery">Delivery Details</Link>
+							<Link to="/terms">Terms & Conditions</Link>
+							<Link to="/privacy">Privacy Policy</Link>
+						</Box>
+						<Box className="foot-category-link">
+							<Box className="foot-category-title">FAQ</Box>
+							<Link to="/account">Account</Link>
+							<Link to="/manage-deliveries">Manage Deliveries</Link>
+							<Link to="/orders">Orders</Link>
+							<Link to="/payments">Payments</Link>
+						</Box>
+						<Box className="foot-category-link">
+							<Box className="foot-category-title">RESOURCES</Box>
+							<Link to="/ebooks">Free eBooks</Link>
+							<Link to="/tutorial">Development Tutorial</Link>
+							<Link to="/blog">How to - Blog</Link>
+							<Link to="/youtube">Youtube Playlist</Link>
+						</Box>
+					</Stack>
+				</Stack>
+
+				<div className="footer-divider"></div>
+
+				<Stack
+					direction="row"
+					justifyContent="space-between"
+					alignItems="center"
+				>
+					<Box className="copyright-txt">
+						Zenvyra © 2000-2023, All Rights Reserved
+					</Box>
+					<Box className="payment-icons">
+						<img src="/icons/visa.svg" alt="visa" />
+						<img src="/icons/mastercard.svg" alt="mastercard" />
+						<img src="/icons/paypal.svg" alt="paypal" />
+						<img src="/icons/apple-pay.svg" alt="applepay" />
+						<img src="/icons/google-pay.svg" alt="googlepay" />
+					</Box>
+				</Stack>
+			</Container>
+		</div>
+	);
 }
