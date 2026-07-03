e8777cbf7dc5d19b60629b21de07fbf839e4afd7
adam01051 <lloki0105@gmail.com>
2026-07-03 22:49:03 +0900

    feat: deploy project via shel and pm2

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+
+#!/bin/bash
+#PRODUCTION
+# git reset --hard
+# git checkout main
+# git pull origin main
+
+npm i yarn -g
+yarn global add serve
+yarn
+yarn build
+pm2 start "yarn run start:prod" --name "ZENVYRA-REACT"
+
+
+# #DEVELOPMENT
+# git reset --hard
+# git checkout dev
+# git pull origin dev
+
+
+# yarn install
+# pm2 start process.config.js --env production
+
+# yarn
+# pm2 start "yarn run start:dev" --name "ZENVYRA"
