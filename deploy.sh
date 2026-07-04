5a60cac927e512fb5a2b7bd0c71d37ea3777f8f0
adam01051 <lloki0105@gmail.com>
2026-07-04 17:28:31 +0900

    fix deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 
 #!/bin/bash
 #PRODUCTION
-# git reset --hard
-# git checkout main
-# git pull origin main
+git reset --hard
+git checkout main
+git pull origin main
 
 npm i yarn -g
 yarn global add serve
 yarn
 yarn build
-pm2 start "yarn run start:prod" --name "ZENVYRA-REACT"
+pm2 start "yarn run start:prod" --name= "ZENVYRA-REACT"
 
 
 # #DEVELOPMENT
