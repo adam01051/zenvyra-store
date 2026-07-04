0bf803ef71de8589af4c67c89faf9c5b74b8085f
adam01051 <lloki0105@gmail.com>
2026-07-04 17:13:03 +0900

    fix: prod process

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
 #PRODUCTION
-# git reset --hard
-# git checkout main
-# git pull origin main
+git reset --hard
+git checkout main
+git pull origin main
 
 npm i yarn -g
 yarn global add serve
